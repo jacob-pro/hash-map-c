64bee66590a31c2eb0f7df4fc676f71e3ec6861f
Jacob Halsey <jacob@jhalsey.com>
2020-06-16 13:11:35 +0100

    spaces

--- hash_map.c
+++ hash_map.c
@@ -8,195 +8,195 @@
 #include <string.h>
 
 typedef struct Node {
-	void *key;
-	hash_t hash;
-	void *value;
-	struct Node *next;
+    void *key;
+    hash_t hash;
+    void *value;
+    struct Node *next;
 } Node;
 
 struct HashMap {
-	hash_t size;		// Number of entries
-	hash_t capacity;	// Must be power of 2 (the length of table)
-	hash_t resize_at;
-	float load_factor;
-	HashFunc hash_func;
-	IsEqualFunc is_equal_func;
-	Node **table;
+    hash_t size;		// Number of entries
+    hash_t capacity;	// Must be power of 2 (the length of table)
+    hash_t resize_at;
+    float load_factor;
+    HashFunc hash_func;
+    IsEqualFunc is_equal_func;
+    Node **table;
 };
 
 // Get the nearest power of two
 // https://www.techiedelight.com/round-next-highest-power-2/
 hash_t power_of_two(hash_t n) {
-	n--;
-	while (n & n - 1) {
-		n = n & n - 1;
-	}
-	return n << 1;
+    n--;
+    while (n & n - 1) {
+        n = n & n - 1;
+    }
+    return n << 1;
 }
 
 HashMap *hashmap_init_with_options(hash_t initial_size, float load_factor, HashFunc hash, IsEqualFunc equal) {
-	HashMap *h = malloc(sizeof(HashMap));
-	h->size = 0;
-	h->capacity = (initial_size > 0) ? power_of_two(initial_size) : power_of_two(HM_DEFAULT_SIZE);
-	h->load_factor = (load_factor > 0 && load_factor < 1) ? load_factor : HM_DEFAULT_FACTOR;
-	h->resize_at = h->capacity * h->load_factor;
-	h->is_equal_func = equal;
-	h->hash_func = hash;
-	h->table = malloc(sizeof(Node*) * h->capacity);
-	for (hash_t i = 0; i < h->capacity; i++) h->table[i] = NULL;
-	return h;
+    HashMap *h = malloc(sizeof(HashMap));
+    h->size = 0;
+    h->capacity = (initial_size > 0) ? power_of_two(initial_size) : power_of_two(HM_DEFAULT_SIZE);
+    h->load_factor = (load_factor > 0 && load_factor < 1) ? load_factor : HM_DEFAULT_FACTOR;
+    h->resize_at = h->capacity * h->load_factor;
+    h->is_equal_func = equal;
+    h->hash_func = hash;
+    h->table = malloc(sizeof(Node*) * h->capacity);
+    for (hash_t i = 0; i < h->capacity; i++) h->table[i] = NULL;
+    return h;
 }
 
 HashMap *hashmap_init_for_pointer_key(hash_t initial_size, float load_factor) {
-	return hashmap_init_with_options(initial_size, load_factor, pointer_hash, pointer_is_equal);
+    return hashmap_init_with_options(initial_size, load_factor, pointer_hash, pointer_is_equal);
 }
 
 HashMap *hashmap_init_for_string_key(hash_t initial_size, float load_factor) {
-	return hashmap_init_with_options(initial_size, load_factor, java_string_hash, string_is_equal);
+    return hashmap_init_with_options(initial_size, load_factor, java_string_hash, string_is_equal);
 }
 
 #define INDEX_FROM_HASH(hash, capacity) \
-	(hash & (capacity - 1))		// the capactiy is always a power of 2 hence why this can be done
+    (hash & (capacity - 1))		// the capactiy is always a power of 2 hence why this can be done
 
 void *hashmap_get_value(HashMap *h, void *key) {
-	if (key == NULL) return NULL;
+    if (key == NULL) return NULL;
 
-	hash_t hash = h->hash_func(key);
-	hash_t idx = INDEX_FROM_HASH(hash, h->capacity);
+    hash_t hash = h->hash_func(key);
+    hash_t idx = INDEX_FROM_HASH(hash, h->capacity);
 
-	Node *node = h->table[idx];
-	while (node != NULL) {
-		if (h->is_equal_func(node->key, key)) return node->value;
-		node = node->next;
-	}
+    Node *node = h->table[idx];
+    while (node != NULL) {
+        if (h->is_equal_func(node->key, key)) return node->value;
+        node = node->next;
+    }
 
-	return NULL;	// Not found
+    return NULL;	// Not found
 }
 
 void expand_table(HashMap *h) {
-	// this should really check for overflow 
-	// but that will not occur until 2^31 which is already too large to be practical
+    // this should really check for overflow
+    // but that will not occur until 2^31 which is already too large to be practical
 
-	hash_t oldSize = h->capacity;
-	hash_t newSize = oldSize << 1;	//Doubles the size
-	
-	Node **oldTable = h->table;
-	Node **newTable = malloc(sizeof(Node*) * newSize);
-	
-	for (hash_t i = 0; i < newSize; i++) newTable[i] = NULL;
+    hash_t oldSize = h->capacity;
+    hash_t newSize = oldSize << 1;	//Doubles the size
 
-	for (hash_t i = 0; i < oldSize; i++) {		//iterate the old table
-		Node *node = oldTable[i];
-		while (node != NULL) {	//iterate entries in each old bucket
+    Node **oldTable = h->table;
+    Node **newTable = malloc(sizeof(Node*) * newSize);
 
-			Node *originalNext = node->next;		//this value may get changed when it is moved
+    for (hash_t i = 0; i < newSize; i++) newTable[i] = NULL;
 
-			hash_t newIdx = INDEX_FROM_HASH(node->hash, newSize);
-			Node *newBucket = newTable[newIdx];	//we may already have put something in the new bucket
-			newTable[newIdx] = node;	//replace this node at the bucket
-			node->next = newBucket;		//then set the next of this node to whatever was/not already there
+    for (hash_t i = 0; i < oldSize; i++) {		//iterate the old table
+        Node *node = oldTable[i];
+        while (node != NULL) {	//iterate entries in each old bucket
 
-			node = originalNext;
-		}
-	}
+            Node *originalNext = node->next;		//this value may get changed when it is moved
 
-	h->table = newTable;
-	h->capacity = newSize;
-	h->resize_at = newSize * h->load_factor;
-	free(oldTable);
+            hash_t newIdx = INDEX_FROM_HASH(node->hash, newSize);
+            Node *newBucket = newTable[newIdx];	//we may already have put something in the new bucket
+            newTable[newIdx] = node;	//replace this node at the bucket
+            node->next = newBucket;		//then set the next of this node to whatever was/not already there
+
+            node = originalNext;
+        }
+    }
+
+    h->table = newTable;
+    h->capacity = newSize;
+    h->resize_at = newSize * h->load_factor;
+    free(oldTable);
 }
 
 void hashmap_set_value(HashMap *h, void *key, void *value) {
-	if (key == NULL) return;
+    if (key == NULL) return;
 
-	hash_t hash = h->hash_func(key);
-	hash_t idx = INDEX_FROM_HASH(hash, h->capacity);
+    hash_t hash = h->hash_func(key);
+    hash_t idx = INDEX_FROM_HASH(hash, h->capacity);
 
-	//If this key is already in the table
-	Node *previous = NULL;
-	Node *eval = h->table[idx];
-	while (eval != NULL) {
-		if (h->is_equal_func(eval->key, key)) {
-			
-			if (value == NULL) {		//remove the eval node
-				
-				if (previous != NULL) {
-					previous->next = eval->next;
-				} else {
-					h->table[idx] = eval->next;
-				}
-				free(eval);
-				h->size--;
-				
+    //If this key is already in the table
+    Node *previous = NULL;
+    Node *eval = h->table[idx];
+    while (eval != NULL) {
+        if (h->is_equal_func(eval->key, key)) {
 
-			} else {			//replace the eval value
-				eval->value = value;
-			}
+            if (value == NULL) {		//remove the eval node
 
-			return;
-		}
-		previous = eval;
-		eval = eval->next;
-	}
+                if (previous != NULL) {
+                    previous->next = eval->next;
+                } else {
+                    h->table[idx] = eval->next;
+                }
+                free(eval);
+                h->size--;
 
-	// See if table should be resized
-	if (h->size >= h->resize_at) {
-		expand_table(h);
-		idx = INDEX_FROM_HASH(hash, h->capacity);		//the capacity will have changed
-	}
 
-	// Create the node
-	Node *new = malloc(sizeof(Node));
-	new->value = value;
-	new->key = key;
-	new->hash = hash;
-	new->next = NULL;
-	if (previous != NULL) {
-		previous->next = new;
-	} else {
-		h->table[idx] = new;
-	}
-	h->size++;
+            } else {			//replace the eval value
+                eval->value = value;
+            }
+
+            return;
+        }
+        previous = eval;
+        eval = eval->next;
+    }
+
+    // See if table should be resized
+    if (h->size >= h->resize_at) {
+        expand_table(h);
+        idx = INDEX_FROM_HASH(hash, h->capacity);		//the capacity will have changed
+    }
+
+    // Create the node
+    Node *new = malloc(sizeof(Node));
+    new->value = value;
+    new->key = key;
+    new->hash = hash;
+    new->next = NULL;
+    if (previous != NULL) {
+        previous->next = new;
+    } else {
+        h->table[idx] = new;
+    }
+    h->size++;
 }
 
 void hashmap_destroy(HashMap *h) {
-	for (hash_t i = 0; i < h->capacity; i++) {
-		Node *n = h->table[i];
-		while (n != NULL) {
-			Node *toFree = n;
-			n = n->next;
-			free(toFree);
-		}
-	}
-	free(h->table);	 // this was the first memory leak in 10 weeks :(
-	free(h);
+    for (hash_t i = 0; i < h->capacity; i++) {
+        Node *n = h->table[i];
+        while (n != NULL) {
+            Node *toFree = n;
+            n = n->next;
+            free(toFree);
+        }
+    }
+    free(h->table);	 // this was the first memory leak in 10 weeks :(
+    free(h);
 }
 
 hash_t hashmap_get_size(HashMap *h) {
-	return h->size;
+    return h->size;
 }
 
 hash_t hashmap_get_capacity(HashMap *h) {
-	return h->capacity;
+    return h->capacity;
 }
 
 bool pointer_is_equal(const void *key1, const void *key2) {
-	return (key1 == key2);
+    return (key1 == key2);
 }
 
 bool string_is_equal(const void *key1, const void *key2) {
-	return (strcmp(key1, key2) == 0);
+    return (strcmp(key1, key2) == 0);
 }
 
 uint32_t pointer_hash(const void *key) {
-	return (uint32_t)key;		//Hashing a pointer is pointless?
+    return (uint32_t)key;		//Hashing a pointer is pointless?
 }
 
 uint32_t java_string_hash(const void *key) {
-	const char *str = key;
-	uint32_t h = 0;
-	for (size_t i = 0; i < strlen(str); i++) {
-		h = 31 * h + str[i];
-	}
-	return h;
+    const char *str = key;
+    uint32_t h = 0;
+    for (size_t i = 0; i < strlen(str); i++) {
+        h = 31 * h + str[i];
+    }
+    return h;
 }
--- hash_map_test.c
+++ hash_map_test.c
@@ -13,116 +13,116 @@ static char *collide1 = "Siblings";
 static char *collide2 = "Teheran";
 
 void test_string_keys() {
-	char *value = "Expected Result";
-	HashMap *h = hashmap_init_for_string_key(HM_DEFAULT_SIZE, HM_DEFAULT_FACTOR);
+    char *value = "Expected Result";
+    HashMap *h = hashmap_init_for_string_key(HM_DEFAULT_SIZE, HM_DEFAULT_FACTOR);
 
-	//Two identical strings
-	//But different addresses in memory to check that it is not the pointer that is being compared
-	//Clang (but not MSVC) seems to intern these strings into the same memory when both are in pointer notation
-	char *key1 = "key";
-	char key2[] = "key";
-	assert(key1 != key2);
-	assert(strcmp(key1, key2) == 0);
+    //Two identical strings
+    //But different addresses in memory to check that it is not the pointer that is being compared
+    //Clang (but not MSVC) seems to intern these strings into the same memory when both are in pointer notation
+    char *key1 = "key";
+    char key2[] = "key";
+    assert(key1 != key2);
+    assert(strcmp(key1, key2) == 0);
 
-	//Set the value using key1
-	hashmap_set_value(h, key1, value);
+    //Set the value using key1
+    hashmap_set_value(h, key1, value);
 
-	//Both keys are the same so they should both return the same value
-	char *test1 = hashmap_get_value(h, key1);
-	char *test2 = hashmap_get_value(h, key2);
-	assert(test1 == value);
-	assert(test2 == value);
+    //Both keys are the same so they should both return the same value
+    char *test1 = hashmap_get_value(h, key1);
+    char *test2 = hashmap_get_value(h, key2);
+    assert(test1 == value);
+    assert(test2 == value);
 
-	hashmap_destroy(h);
+    hashmap_destroy(h);
 }
 
 void test_deletion() {
-	char *value = "value";
-	char *key = "key";
-	
-	HashMap *h = hashmap_init_for_string_key(HM_DEFAULT_SIZE, HM_DEFAULT_FACTOR);
-	assert(hashmap_get_size(h) == 0);
+    char *value = "value";
+    char *key = "key";
 
-	//Set the value
-	hashmap_set_value(h, key, value);
-	assert(hashmap_get_size(h) == 1);
-	char *result = hashmap_get_value(h, key);
-	assert(result == value);
+    HashMap *h = hashmap_init_for_string_key(HM_DEFAULT_SIZE, HM_DEFAULT_FACTOR);
+    assert(hashmap_get_size(h) == 0);
 
-	//Delete the value
-	hashmap_set_value(h, key, NULL);
-	assert(hashmap_get_size(h) == 0);
-	char *deleted = hashmap_get_value(h, key);
-	assert(deleted == NULL);
+    //Set the value
+    hashmap_set_value(h, key, value);
+    assert(hashmap_get_size(h) == 1);
+    char *result = hashmap_get_value(h, key);
+    assert(result == value);
 
-	hashmap_destroy(h);
+    //Delete the value
+    hashmap_set_value(h, key, NULL);
+    assert(hashmap_get_size(h) == 0);
+    char *deleted = hashmap_get_value(h, key);
+    assert(deleted == NULL);
+
+    hashmap_destroy(h);
 }
 
 void test_collision() {
-	char *val1 = "val1";
-	char *val2 = "val2";
-	char *result;
-	HashMap *h = hashmap_init_for_string_key(HM_DEFAULT_SIZE, HM_DEFAULT_FACTOR);
+    char *val1 = "val1";
+    char *val2 = "val2";
+    char *result;
+    HashMap *h = hashmap_init_for_string_key(HM_DEFAULT_SIZE, HM_DEFAULT_FACTOR);
 
-	//Confirm that the values do indeed collide
-	hash_t one = java_string_hash(collide1);
-	hash_t two = java_string_hash(collide2);
-	assert(one == two);
+    //Confirm that the values do indeed collide
+    hash_t one = java_string_hash(collide1);
+    hash_t two = java_string_hash(collide2);
+    assert(one == two);
 
-	//Test that both values can be sucessfully stored and retrieved
-	hashmap_set_value(h, collide1, val1);
-	hashmap_set_value(h, collide2, val2);
+    //Test that both values can be sucessfully stored and retrieved
+    hashmap_set_value(h, collide1, val1);
+    hashmap_set_value(h, collide2, val2);
 
-	result = hashmap_get_value(h, collide1);
-	assert(result == val1);
+    result = hashmap_get_value(h, collide1);
+    assert(result == val1);
 
-	result = hashmap_get_value(h, collide2);
-	assert(result == val2);
+    result = hashmap_get_value(h, collide2);
+    assert(result == val2);
 
-	//Try removing the first value
-	hashmap_set_value(h, collide1, NULL);
+    //Try removing the first value
+    hashmap_set_value(h, collide1, NULL);
 
-	result = hashmap_get_value(h, collide1);
-	assert(result == NULL);
+    result = hashmap_get_value(h, collide1);
+    assert(result == NULL);
 
-	result = hashmap_get_value(h, collide2);
-	assert(result == val2);
+    result = hashmap_get_value(h, collide2);
+    assert(result == val2);
 
-	hashmap_destroy(h);
+    hashmap_destroy(h);
 }
 
 void test_growth() {
 
-	// Create a table with initial size 4. 
-	// The load factor of 0.5 means it will need to grow to size 8 on the 3rd entry
-	// And then again to size 16 on the 5th entry
-	HashMap *h = hashmap_init_for_string_key(4, 0.5);
-	assert(hashmap_get_capacity(h) == 4);
+    // Create a table with initial size 4.
+    // The load factor of 0.5 means it will need to grow to size 8 on the 3rd entry
+    // And then again to size 16 on the 5th entry
+    HashMap *h = hashmap_init_for_string_key(4, 0.5);
+    assert(hashmap_get_capacity(h) == 4);
 
-	//Use the collision values at the start to check that they do not get broken after the expansion
-	char *keys[8] = { collide1, collide2, "three", "four", "five", "six", "seven", "eight" };
-	char *values[8] = { "a", "b", "c", "d", "e", "f", "g", "h" };
+    //Use the collision values at the start to check that they do not get broken after the expansion
+    char *keys[8] = { collide1, collide2, "three", "four", "five", "six", "seven", "eight" };
+    char *values[8] = { "a", "b", "c", "d", "e", "f", "g", "h" };
 
-	for (size_t i = 0; i < 8; i++) {
-		hashmap_set_value(h, keys[i], values[i]);
-		if (i == 2) assert(hashmap_get_capacity(h) == 8);
-		if (i == 4) assert(hashmap_get_capacity(h) == 16);
-	}
+    for (size_t i = 0; i < 8; i++) {
+        hashmap_set_value(h, keys[i], values[i]);
+        if (i == 2) assert(hashmap_get_capacity(h) == 8);
+        if (i == 4) assert(hashmap_get_capacity(h) == 16);
+    }
 
-	for (size_t i = 0; i < 8; i++) {
-		char *result = hashmap_get_value(h, keys[i]);
-		assert(result == values[i]);
-	}
+    for (size_t i = 0; i < 8; i++) {
+        char *result = hashmap_get_value(h, keys[i]);
+        assert(result == values[i]);
+    }
 
-	hashmap_destroy(h);
+    hashmap_destroy(h);
 }
 
 int main() {
 
-	test_string_keys();
-	test_deletion();
-	test_collision();
-	test_growth();
+    test_string_keys();
+    test_deletion();
+    test_collision();
+    test_growth();
 
-	return EXIT_SUCCESS;
+    return EXIT_SUCCESS;
 }
